29c69746c40221818c8f6428fb0979d4e87e8c96
Maha Alharbi <m.a.alharbi995@gmail.com>
2022-01-16 14:27:51 +0300

    countNumbers onscroll

--- component1/main.js
+++ component1/main.js
@@ -1,10 +1,33 @@
-let section = document.querySelector(".three");
-let spans = document.querySelectorAll(".progress span");
+// let section3 = document.querySelector(".three");
+// let spans = document.querySelectorAll(".progress span");
+
+// window.onscroll = function(){
+//     if(window.scrollY >= section3.offsetTop - 250){
+//         spans.forEach((span)=>{
+//         span.style.width=span.dataset.width;
+//         })
+//     }
+// }
+////////////////////////////////
+let section4 = document.querySelector(".four");
+let nums = document.querySelectorAll(".nums .num");
+let started = false;
 
 window.onscroll = function(){
-    if(window.scrollY >= section.offsetTop - 250){
-        spans.forEach((span)=>{
-            span.style.width=span.dataset.width;
-        })
-    }
+  if(window.scrollY >= section4.offsetTop - 250 )  {
+  if(!started){
+  nums.forEach((num)=> countNumbers(num))
+ } started = true;
+}
+}
+
+function countNumbers(el){
+    let goal = el.dataset.goal;
+    let count = setInterval(()=>{
+        el.textContent++;
+        if(el.textContent == goal){
+            clearInterval(count);
+            
+        }
+    },2000/goal)
 }
