148da0104624f219bc7dcc01fb607b82f6c31bcf
Maha Alharbi <m.a.alharbi995@gmail.com>
2022-01-17 11:33:21 +0300

    count input & progress

---
+++ countInputChar-component3/main.js
@@ -0,0 +1,14 @@
+let input = document.querySelector("input");
+let progress = document.querySelector(".progress");
+let count = document.querySelector(".count");
+let maxLength = input.getAttribute("maxlength");
+
+count.innerHTML = maxLength;
+
+// event oninput
+input.oninput = function(){
+    count.innerHTML = maxLength - this.value.length;
+    count.innerHTML == 0 ? count.classList.add("zero") : count.classList.remove("zero");
+    // set the progress 
+    progress.style.width = `${(this.value.length * 100)/maxLength}%`;
+}
